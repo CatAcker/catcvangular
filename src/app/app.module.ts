af9a72ffb765f8850d6a6c0988ca79cfdfa0d0f7
Cathryn Ackerman <catacker46@gmail.com>
2020-09-01 23:27:43 +0200

    remove components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,10 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ServerComponent } from './server/server.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    ServerComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
