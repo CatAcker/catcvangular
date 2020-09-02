4adf0e366abd67db1e167dc1549fdf4878f2bf09
Cathryn Ackerman <catacker46@gmail.com>
2020-09-02 16:21:30 +0200

    recipe practise

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -6,7 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
-
+  ingredients = [];
+  
   constructor() { }
 
   ngOnInit(): void {
