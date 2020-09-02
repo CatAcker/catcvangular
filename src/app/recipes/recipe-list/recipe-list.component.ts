890b686821a8aa0bdc46bf69043c628662131538
Cathryn Ackerman <catacker46@gmail.com>
2020-09-02 16:04:08 +0200

    array Commit

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -8,7 +8,12 @@ import { Recipe } from '../recipe.model';
 })
 export class RecipeListComponent implements OnInit {
   recipes: Recipe[] =[
-    new Recipe('A test recipe', 'This is a test','https://www.biggerbolderbaking.com/wp-content/uploads/2019/07/15-Minute-Pizza-WS-Thumbnail.png')
+    new Recipe('A test recipe',
+     'This is a test',
+     'https://www.biggerbolderbaking.com/wp-content/uploads/2019/07/15-Minute-Pizza-WS-Thumbnail.png'),
+     new Recipe('A 2nd test recipe',
+     'This is a 2nd test',
+     'https://www.biggerbolderbaking.com/wp-content/uploads/2019/07/15-Minute-Pizza-WS-Thumbnail.png')
   ];
 
   constructor() { }
