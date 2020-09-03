3880857c2fe4fd66fc0b599e4a1637619d9a37c4
Cathryn Ackerman <catacker46@gmail.com>
2020-09-03 14:57:39 +0200

    add animations Udemy proj

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,26 @@
 import { Component } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { trigger } from '@angular/animations';
+import { animationFrame } from 'rxjs/internal/scheduler/animationFrame';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  animations: [
+    trigger('divState', [
+      
+    ])
+  ]
 })
 export class AppComponent {
+  list = ['Milk', 'Sugar', 'Bread'];
+
+  onAdd(item) {
+    this.list.push(item);
+  }
+
+  onDelete(item){
+    this.list.splice(this.list.indexOf(item),1);
+  }
 }
