5652179e90b6f105621f702677d8caabfd487bab
Cathryn Ackerman <catacker46@gmail.com>
2020-09-04 10:39:02 +0200

    remove all

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderComponent } from './header/header.component';
+import { DefaultviewComponent } from './defaultview/defaultview.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    DefaultviewComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/defaultview/defaultview.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-defaultview',
+  templateUrl: './defaultview.component.html',
+  styleUrls: ['./defaultview.component.css']
+})
+export class DefaultviewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
