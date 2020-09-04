cc8598010490c1169d6d0153da625d4be3dec1a7
Cathryn Ackerman <catacker46@gmail.com>
2020-09-04 13:13:26 +0200

    add new class in default components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,11 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HeaderComponent } from './header/header.component';
 import { DefaultviewComponent } from './defaultview/defaultview.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
     DefaultviewComponent
   ],
   imports: [
--- src/app/defaultview/defaultview.component.ts
+++ src/app/defaultview/defaultview.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { IgxNavigationDrawerComponent } from "igniteui-angular";
 
 @Component({
   selector: 'app-defaultview',
   templateUrl: './defaultview.component.html',
   styleUrls: ['./defaultview.component.css']
 })
-export class DefaultviewComponent implements OnInit {
+export class DefaultviewComponent {
+  public navItems = [
+    { name: "account_circle", text: "Avatar" },
+    { name: "error", text: "Badge" },
+    { name: "group_work", text: "Button Group" }
+];
 
-  constructor() { }
+public selected = "Avatar";
 
-  ngOnInit(): void {
-  }
+@ViewChild(IgxNavigationDrawerComponent, { static: true })
+public drawer: IgxNavigationDrawerComponent;
 
+public navigate(item) {
+    this.selected = item.text;
+    this.drawer.close();
+}
 }
