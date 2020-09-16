b9865dfce484925bea31ac8772ba59562081f67c
Cathryn Ackerman <catacker46@gmail.com>
2020-09-16 10:25:19 +0200

    make progress prettier

--- src/app/skills-page/skills.js
+++ src/app/skills-page/skills.js
@@ -1,9 +1,12 @@
-var lang = {
-    "html": "100%",
+let lang = {
+    "html": "75%",
     "css": "90%",
     "javascript": "70%",
-    "php": "55%",
-    "angular": "65%"
+    "C++": "55%",
+    "angular": "60%",
+    "java": "85%",
+    "csharp": "100%",
+    "delphi": "60%"
 };
 
 var multiply = 4;
